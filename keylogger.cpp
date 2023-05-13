627feacde07df4fda0f016fba1ce075dcb9328c5
Jin <ghkdtpwls121@hanyang.ac.kr>
2023-05-13 10:37:38 +0900

    Add feature: Capture input in 1/60 second intervals

--- keylogger.cpp
+++ keylogger.cpp
@@ -1,201 +1,132 @@
 #include <iostream>
+#include <chrono>
 #include <windows.h>
-#include <tchar.h>
+#include <fstream>
+#include <conio.h> // for _kbhit() and _getch()
 
 #define LK 90
 #define RK 88
 #define LP 65
 #define RP 83
 
+#define KEY_DOWN 80
+#define KEY_UP 72
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+
 using namespace std;
 
-bool is_down_2 = false;
-bool is_down_4 = false;
-bool is_down_6 = false;
-bool is_down_8 = false;
-bool is_down_LK = false;
-bool is_down_RK = false;
-bool is_down_LP = false;
-bool is_down_RP = false;
+int main()
+{
+    char input;
+    // [LP, RP, LK, RK, DOWN, LEFT, RIGHT, UP]
+    bool is_down[8] = {false, false, false, false, false, false, false, false};
 
-bool is_key_printed[9] = {false, false, false, false, false, false, false, false, false};
-bool is_attack_printed[6] = {false, false, false, false, false, false};
+    // Open txt file to write inputs
+    ofstream myfile;
+    myfile.open ("inputs.txt");
+    
+    
+    cout << "Press any key to start" << '\n';
+    while(!_kbhit()){}
 
-void init_key_printed(){
-    for (int i = 0; i < 9; i++){
-        is_key_printed[i] = false;
-    }
-}
+    cout << "Start scanning after 2 seconds" << '\n';
+    Sleep(2000);
+    cout << "Start scanning!!!!!!" << '\n';
 
-LRESULT CALLBACK keyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
-	PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT) (lParam);
-	
-
-
-	// If key is being pressed
-	if (wParam == WM_KEYDOWN) {
-		switch (p->vkCode) {
-
-			//  Move keys
-			case VK_LEFT:
-                is_down_4 = true;
+    
+    // Scan input for 2 seconds
+    for(int i = 0; i < 60; i++){
+        // Scan keyboard input for 1/60 seconds
+        auto start_time = chrono::high_resolution_clock::now();
+        // loop this scanning process until 1/60 seconds has passed
+        while(true){
+            if (chrono::duration_cast<chrono::duration<double>>(chrono::high_resolution_clock::now() - start_time).count() >= 2.0/60.0) {
                 break;
+            }
 
-			case VK_RIGHT:
-                is_down_6 = true;
-                break;
-
-			case VK_UP:
-                is_down_8 = true;
-                break;
-
-			case VK_DOWN:
-                is_down_2 = true;
-                break;
-
-			//  Attack keys
-			case LK:
-                is_down_LK = true;
-                break;
-
-			case RK:
-                is_down_RK = true;
-                break;
-
-            case LP:
-                is_down_LP = true;
-                break;
-
-            case RP:
-                is_down_RP = true;
-                break;
-		}
-	}
-
-    if (wParam == WM_KEYUP) {
-        switch (p->vkCode) {
-
-            //  Move keys
-            case VK_DOWN:
-                is_down_2 = false;
-                init_key_printed();
-                break;
-
-            case VK_UP:
-                is_down_8 = false;
-                init_key_printed();
-                break;
-
-            case VK_RIGHT:
-                is_down_6 = false;
-                init_key_printed();
-                break;
+            if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+                is_down[4] = true;
+            }
+            if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
+                is_down[5] = true;
+            }
+            if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+                is_down[6] = true;
+            }
+            if (GetAsyncKeyState(VK_UP) & 0x8000) {
+                is_down[7] = true;
+            }
             
-            case VK_LEFT:
-                is_down_4 = false;
-                init_key_printed();
-
-            //  Attack keys
-            case LK:
-                is_down_LK = false;
-                is_attack_printed[0] = false;
-                is_attack_printed[2] = false;
-                break;
-
-			case RK:
-                is_down_RK = false;
-                is_attack_printed[0] = false;
-                is_attack_printed[3] = false;
-                break;
-
-            case LP:
-                is_down_LP = false;
-                is_attack_printed[1] = false;
-                is_attack_printed[4] = false;
-                break;
-
-            case RP:
-                is_down_RP = false;
-                is_attack_printed[1] = false;
-                is_attack_printed[5] = false;
-                break;
+            if (GetAsyncKeyState(LP) & 0x8000) {
+                is_down[0] = true;
+            }
+            if (GetAsyncKeyState(RP) & 0x8000) {
+                is_down[1] = true;
+            }
+            if (GetAsyncKeyState(LK) & 0x8000) {
+                is_down[2] = true;
+            }
+            if (GetAsyncKeyState(RK) & 0x8000) {
+                is_down[3] = true;
+            }
         }
+        
+        if (is_down[0] && is_down[1]){
+            myfile << "<AP> ";            
+        }
+        else if(is_down[2] && is_down[3]){
+            myfile << "<AK> ";
+        }
+        else if(is_down[0]){
+            myfile << "<LP> ";
+        }
+        else if(is_down[1]){
+            myfile << "<RP> ";
+        }
+        else if(is_down[2]){
+            myfile << "<LK> ";
+        }
+        else if(is_down[3]){
+            myfile << "<RK> ";
+        }
+        else{
+            myfile << "<  > ";
+        }
+        
+        if(is_down[4] && is_down[5]){
+            myfile << "<1>" << '\n';
+        }
+        else if(is_down[4] && is_down[6]){
+            myfile << "<3>" << '\n';
+        }
+        else if(is_down[7] && is_down[5]){
+            myfile << "<7>" << '\n';
+        }
+        else if(is_down[7] && is_down[6]){
+            myfile << "<9>" << '\n';
+        }
+        else if(is_down[4]){
+            myfile << "<2>" << '\n';
+        }
+        else if(is_down[5]){
+            myfile << "<4>" << '\n';
+        }
+        else if(is_down[6]){
+            myfile << "<6>" << '\n';
+        }
+        else if(is_down[7]){
+            myfile << "<8>" << '\n';
+        }
+        else{
+            myfile << "< >" << '\n';
+        }
+
+
+        for(int i = 0; i < 8; i++)  is_down[i] = false;
     }
 
-    if (is_down_2 && is_down_4 && !is_key_printed[0]){
-        cout << "\r" << "<1> ";
-        is_key_printed[0] = true;
-    }
-    else if (is_down_2 && is_down_6 && !is_key_printed[2]){
-        cout << "\r" << "<3> ";
-        is_key_printed[2] = true;
-    }
-    else if (is_down_8 && is_down_4 && !is_key_printed[6]){
-        cout << "\r" << "<7> ";
-        is_key_printed[6] = true;
-    }
-    else if (is_down_8 && is_down_6 && !is_key_printed[8]){
-        cout << "\r" << "<9> ";
-        is_key_printed[8] = true;
-    }
-    else if (is_down_2 && !is_key_printed[1]){
-        cout << "\r" << "<2> ";
-        is_key_printed[1] = true;
-    }
-    else if (is_down_4 && !is_key_printed[3]){
-        cout << "\r" << "<4> ";
-        is_key_printed[3] = true;
-    }
-    else if (is_down_6 && !is_key_printed[5]){
-        cout << "\r" << "<6> ";
-        is_key_printed[5] = true;
-    }
-    else if (is_down_8 && !is_key_printed[7]){
-        cout << "\r" << "<8> ";
-        is_key_printed[7] = true;
-    }
-
-    
-    if (is_down_LK && is_down_RK && !is_attack_printed[0]){
-        cout << "\r\n" << "<AK> ";
-        is_attack_printed[0] = true;
-    }
-    else if (is_down_LP && is_down_RP && !is_attack_printed[1]){
-        cout << "\r\n" << "<AP> ";
-        is_attack_printed[1] = true;
-    }
-    else if (is_down_LK && !is_attack_printed[2]){
-        cout << "\r\n" << "<LK> ";
-        is_attack_printed[2] = true;
-    }
-    else if (is_down_RK && !is_attack_printed[3]){
-        cout << "\r\n" << "<RK> ";
-        is_attack_printed[3] = true;
-    }
-    else if (is_down_LP && !is_attack_printed[4]){
-        cout << "\r\n" << "<LP> ";
-        is_attack_printed[4] = true;
-    }
-    else if (is_down_RP && !is_attack_printed[5]){
-        cout << "\r\n" << "<RP> ";
-        is_attack_printed[5] = true;
-    }
-    
-
-    
-    
-	return CallNextHookEx(NULL, nCode, wParam, lParam);
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-
-	// Set windows hook
-	HHOOK keyboardHook = SetWindowsHookEx(
-		WH_KEYBOARD_LL,
-		keyboardHookProc,
-		hInstance,
-		0);
-
-	MessageBox(NULL, _T("Press OK to stop logging."), _T("Information"), MB_OK);
-	return 0;
+    cout << "Done!" << '\n';
+    myfile.close();
+    return 0;
 }
